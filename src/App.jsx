41ac035898696ef488edb0aca2fda796aaf67032
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-04-15 10:17:16 +0200

    add all pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import './App.css'
+import { Routes, Route, NavLink } from 'react-router-dom'
+import HomePage from './pages/homePage/HomePage'
+import NotFoundPage from './pages/notFoundPage/NotFoundPage'
 
 function App() {
+    return (
+        <div>
+            <nav>
+                <NavLink to = "/">Home</NavLink>
+                <NavLink to = "/Users">Users</NavLink>
+            </nav>
+        <Routes>
+            <Route path = "/" element = {<HomePage/>}/>
+            <Route path = "/Users" element = {<div>Users Page</div>}/>
+            <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
+        </div>
+    )
   
 }
 
---
+++ src/pages/notFoundPage/NotFoundPage.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const NotFoundPage = () => {
+    
+    return (
+        <div>
+             <div>Not found page</div>
+             <Link to="/">Go Home</Link>
+        </div>
+    );
+    
+  }
+  
+  export default NotFoundPage;
